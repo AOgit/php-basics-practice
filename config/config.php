a5452aaa38afcb66f0309665e59cc22ec4eb7b52
Oleksandr <git.hub@ukr.net>
2025-01-20 23:00:50 +0100

    done! Практика PHP для начинающих. Урок 5. Структура приложения. Создание базы данных (БД)

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define("ROOT", dirname(__DIR__));
+define("PUBLIC", ROOT . '/public');
+define("CORE", ROOT . '/core');
+define("APP", ROOT . '/app');
+define("CONFIG", ROOT .'/config');
+define("CONTROLLERS", APP . '/controllers');
+define("VIEWS", APP  . '/views');
+define("PATH",  'https://php-kudlay');
--- public/index.php
+++ public/index.php
@@ -1,24 +1,10 @@
 <?php
 
-define("ROOT", dirname(__DIR__));
-define("PUBLIC", ROOT . '/public');
-define("CORE", ROOT . '/core');
-define("APP", ROOT . '/app');
-define("CONTROLLERS", APP . '/controllers');
-define("VIEWS", APP  . '/views');
-define("PATH",  'https://php-kudlay');
-
+require  dirname(__DIR__) . "/config/config.php";
 
 
 require CORE . "/funcs.php";
 
-$uri = trim(parse_url($_SERVER['REQUEST_URI'])['path'], '/');
-
-if ($uri === "")
-    require CONTROLLERS . "/index.php";
-else if ($uri ==="about")
-    require CONTROLLERS . "/" . $uri . ".php"; 
-else
-    abort();
+require CORE . "/router.php";
 
 
